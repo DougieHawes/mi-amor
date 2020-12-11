3a5d8822f91344072d016404b44aae853bfee5e0
DougieHawes <dougiehawes@hotmail.com>
2020-12-11 13:53:00 +0000

    initial commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./style.min.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1 className="title">Mi Amor</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
